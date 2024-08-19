257f618a02c68485828b7231249daa803669777d
Himanshu Sharma <155414563+himanshus1904@users.noreply.github.com>
2024-08-19 11:45:19 +0530

    Update register.py

--- register.py
+++ register.py
@@ -30,43 +30,40 @@ def register():
 
     if st.button("Submit"):
         st.write("New Username",new_username)
-        if new_username in user_data:
-            st.error("Username already exists")
-        else:
-            user_data[new_username] = {'password': new_password, 'agent_name': agent_name, 'org_name': org_name }
-            save_user_data(user_data)
-            st.success("Registration successful! Please login.")
-            st.session_state.page = "Login"
-            st.session_state.username = new_username
-            st.session_state.agent_name = agent_name
-            st.session_state.org_name = org_name
-            st.session_state.description = description
-            st.session_state.sql_path = sql_path
-            st.session_state.table_name = table_name
-            st.session_state.website = website
-            st.session_state.documents = documents
-            st.session_state.sec_key = sec_key
-            st.session_state.page = 'chat'
+        user_data[new_username] = {'password': new_password, 'agent_name': agent_name, 'org_name': org_name }
+        save_user_data(user_data)
+        st.success("Registration successful! Please login.")
+        st.session_state.page = "Login"
+        st.session_state.username = new_username
+        st.session_state.agent_name = agent_name
+        st.session_state.org_name = org_name
+        st.session_state.description = description
+        st.session_state.sql_path = sql_path
+        st.session_state.table_name = table_name
+        st.session_state.website = website
+        st.session_state.documents = documents
+        st.session_state.sec_key = sec_key
+        st.session_state.page = 'chat'
 
-            sql_agent = SQLAgent()
-            website_agent = WebsiteAgent()
-            pdf_agent = PDFAgent()
+        sql_agent = SQLAgent()
+        website_agent = WebsiteAgent()
+        pdf_agent = PDFAgent()
 
-            sql_data = sql_agent.run(st.session_state.sql_path, st.session_state.table_name) if (
-                st.session_state.sql_path) else []
-            website_data = website_agent.run(st.session_state.website) if st.session_state.website else ""
-            pdf_data = pdf_agent.run(st.session_state.documents) if st.session_state.documents else []
+        sql_data = sql_agent.run(st.session_state.sql_path, st.session_state.table_name) if (
+            st.session_state.sql_path) else []
+        website_data = website_agent.run(st.session_state.website) if st.session_state.website else ""
+        pdf_data = pdf_agent.run(st.session_state.documents) if st.session_state.documents else []
 
-            # Combine results and query LLM
-            combined_data = combine_results(sql_data, website_data, pdf_data)
-            process_text(new_username, combined_data)
-            dir_list = os.listdir(os.getcwd()) 
-            st.write("Files and directories'", dir_list)
-            st.write("All process done")
+        # Combine results and query LLM
+        combined_data = combine_results(sql_data, website_data, pdf_data)
+        process_text(new_username, combined_data)
+        dir_list = os.listdir(os.getcwd()) 
+        st.write("Files and directories'", dir_list)
+        st.write("All process done")
 
-            # model = genai.GenerativeModel('gemini-pro')
-            st.session_state.llm = genai.GenerativeModel('gemini-1.5-pro-001')
-            # os.environ["HUGGINGFACEHUB_API_TOKEN"] = sec_key repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
-            # st.session_state.llm = HuggingFaceEndpoint(repo_id=repo_id, max_length=128, temperature=0.7,
-            # token=sec_key)
-            st.session_state.page = 'chat'
+        # model = genai.GenerativeModel('gemini-pro')
+        st.session_state.llm = genai.GenerativeModel('gemini-1.5-pro-001')
+        # os.environ["HUGGINGFACEHUB_API_TOKEN"] = sec_key repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+        # st.session_state.llm = HuggingFaceEndpoint(repo_id=repo_id, max_length=128, temperature=0.7,
+        # token=sec_key)
+        st.session_state.page = 'chat'
